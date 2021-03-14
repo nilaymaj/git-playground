8c11387e63a1d10f7f64f7b7d5a6d0bfa92aa3e8
Nilay Majorwar <nilaymajorwar@gmail.com>
2021-03-14 16:28:48 +0530

    Minor fix in file manager UI

--- src/ui/panes/files-manager/file-item.tsx
+++ src/ui/panes/files-manager/file-item.tsx
@@ -7,12 +7,17 @@ type Props = {
 };
 
 export const FileItem = (props: Props) => {
+  const { contentToken, version } = props.file;
+  const fileInfo = `${contentToken.slice(-12)} v${version}`;
+
   return (
     <div className='file-item'>
       <div className='icon'>
         <IoDocumentOutline />
       </div>
-      <div className='name'>{props.name}</div>
+      <div className='name' title={fileInfo}>
+        {props.name}
+      </div>
     </div>
   );
 };
