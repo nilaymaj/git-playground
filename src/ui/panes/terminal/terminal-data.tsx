39c06bec3f5bbe3665fb070f041ee4b616fb2933
Nilay Majorwar <nilaymajorwar@gmail.com>
2021-03-14 16:28:37 +0530

    Add scroll-to-bottom in terminal

--- src/ui/panes/terminal/terminal-data.tsx
+++ src/ui/panes/terminal/terminal-data.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { TerminalHistory } from './terminal';
 import TerminalRow from './terminal-row';
 
@@ -6,8 +7,17 @@ type Props = {
 };
 
 export const TerminalData = (props: Props) => {
+  const containerRef = React.useRef<HTMLDivElement | null>(null);
+
+  // Scroll to bottom when history changes
+  React.useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+    container.scrollTop = container.scrollHeight;
+  }, [props.data]);
+
   return (
-    <div className='terminal-data'>
+    <div className='terminal-data' ref={(r) => (containerRef.current = r)}>
       {Array.from({ length: props.data.size }).map((_, idx) => {
         const dataLine = props.data.get(idx);
         if (!dataLine) throw new Error(`This shouldn't happen.`);
