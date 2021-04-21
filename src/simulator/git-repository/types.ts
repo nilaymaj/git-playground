a7b136584af9c4cfbbba058a26800b458a3fcc74
Nilay Majorwar <nilaymajorwar@gmail.com>
2021-04-21 16:43:13 +0530

    Pull HEAD related logic into separate class

--- src/simulator/git-repository/types.ts
+++ src/simulator/git-repository/types.ts
@@ -1,26 +1,7 @@
+import GitHead from './head';
 import IndexFile from './index-file';
 import ObjectStorage from './object-storage';
-import { GitObjectAddress } from './object-storage/types';
-import RefStorage, { RefPath } from './ref-storage';
-
-// Detached head - points to commit object
-interface DetachedHead {
-  isDetached: true;
-  destination: GitObjectAddress;
-}
-
-// Stable head - points to local ref
-interface StableHead {
-  isDetached: false;
-  destination: RefPath;
-}
-
-/**
- * Represents Git HEAD. Can be
- * - stable (pointing towards local ref)
- * - detached (pointing directly to commit)
- */
-export type GitHead = StableHead | DetachedHead;
+import RefStorage from './ref-storage';
 
 /**
  * Represents the Git repository of a single sandbox.
